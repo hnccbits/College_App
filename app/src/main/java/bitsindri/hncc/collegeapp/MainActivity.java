1ac10659e4af0b756ce545bccba6c2af2e386347
Anubhaw19 <anubhawsharma99@gmail.com>
2020-10-31 18:07:10 +0530

    StoryView Added

--- app/src/main/java/bitsindri/hncc/collegeapp/MainActivity.java
+++ app/src/main/java/bitsindri/hncc/collegeapp/MainActivity.java
@@ -1,14 +1,101 @@
 package bitsindri.hncc.collegeapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import xute.storyview.StoryModel;
+import xute.storyview.StoryView;
 
 public class MainActivity extends AppCompatActivity {
+    //StoryView storyView;   // get the object for StoryView
+
+    RecyclerView recyclerView;
+    ArrayList<UserList> data=new ArrayList<UserList>();
+    //ViewPager2 viewpager;
+    PageAdapter pageAdapter;
+    ArrayList<ArrayList<StoryModel>> user_list=new ArrayList<ArrayList<StoryModel>>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView=findViewById(R.id.revyclerview);
+
+        //viewpager=findViewById(R.id.viewpager2);
+//
+//        storyView = findViewById(R.id.storyView); // find the XML view using findViewById
+//        storyView.resetStoryVisits(); // reset the storyview
+//
+        ArrayList<StoryModel> StoriesList = new ArrayList<>();  // create a Array list of Stories
+
+        StoriesList.add(new StoryModel("https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80","Status 1","Yesterday"));
+        StoriesList.add(new StoryModel("https://www.bigstockphoto.com/images/homepage/module-6.jpg","Status 2","10:15 PM"));
+        StoriesList.add(new StoryModel("https://www.gettyimages.com/gi-resources/images/500px/983794168.jpg","Satus 3","Today,2:31 PM"));
+//        storyView.setImageUris(StoriesList);  // finally set the stories to storyview
+        user_list.add(StoriesList);
+        data.add(new UserList(user_list,"User1","Today,11:00am"));
+        StoriesList.clear(); // claering previous data from arraylist.
+
+        StoriesList.add(new StoryModel("https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80","Status 1","Yesterday"));
+        StoriesList.add(new StoryModel("https://www.bigstockphoto.com/images/homepage/module-6.jpg","Status 2","10:15 PM"));
+        StoriesList.add(new StoryModel("https://www.gettyimages.com/gi-resources/images/500px/983794168.jpg","Satus 3","Today,2:31 PM"));
+        user_list.add(StoriesList);
+        data.add(new UserList(user_list,"User2","Today,12:00am"));
+        // StoriesList.clear(); // claering previous data from arraylist.
+
+        user_list.add(StoriesList);
+        data.add(new UserList(user_list,"User3","Today,01:00pm"));
+        user_list.add(StoriesList);
+        data.add(new UserList(user_list,"User4","yestarday"));
+        user_list.add(StoriesList);
+        data.add(new UserList(user_list,"User5","Today,09:00am"));
+        user_list.add(StoriesList);
+        data.add(new UserList(user_list,"User6","yestarday"));
+
+
+
+        pageAdapter=new PageAdapter(data);
+
+//        viewpager.setClipToPadding(false);
+//        viewpager.setClipChildren(false);
+//        viewpager.setOffscreenPageLimit(3);
+//        viewpager.getChildAt(0).setOverScrollMode(viewpager.OVER_SCROLL_ALWAYS);
+////        viewpager.getChildAt(0).setOverScrollMode(viewpager.OVER_SCROLL_NEVER);
+//        viewpager.setAdapter(pageAdapter);
+//
+//        CompositePageTransformer transformer=new CompositePageTransformer();
+//        transformer.addTransformer(new MarginPageTransformer(0));
+//        transformer.addTransformer(new ViewPager2.PageTransformer() {
+//            @Override
+//            public void transformPage(@NonNull View page, float position) {
+//
+//                float r = 1 - Math.abs(position);
+//                //page.setScaleY(0.8f);
+//
+//                page.setScaleY(0.8f + r * 0.05f);
+//
+//            }
+//        });
+//        viewpager.setPageTransformer(transformer);
+
+
+        //RECYCLER VIEW
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        manager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(pageAdapter);
+
+
     }
 }
---
+++ app/src/main/java/bitsindri/hncc/collegeapp/PageAdapter.java
@@ -0,0 +1,86 @@
+package bitsindri.hncc.collegeapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import xute.storyview.StoryView;
+
+public class PageAdapter extends RecyclerView.Adapter<PageAdapter.ViewHolder> {
+    ArrayList<UserList> data ;
+    public PageAdapter(ArrayList<UserList>  data)
+    {
+
+        this.data=data;
+
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.story_list,parent,false);
+        return  new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        if(position==0)
+        {
+            holder.addStroy.setVisibility(View.VISIBLE);
+            holder.textTime.setText(null);
+            holder.storyView.setVisibility(View.GONE);
+            holder.dp.setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            int index=position-1;
+            holder.textName.setText(data.get(index).getUserName());
+            holder.textTime.setText(data.get(index).getTime());
+            holder.storyView.resetStoryVisits();
+            holder.storyView.setImageUris(data.get(index).getUser().get(index));
+        }
+        if(position==data.size())
+        {
+            holder.textName.setText(data.get(position).getUserName());
+            holder.textTime.setText(data.get(position).getTime());
+            holder.storyView.resetStoryVisits();
+            holder.storyView.setImageUris(data.get(position).getUser().get(position));
+        }
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        //ImageView imageView;
+        TextView textName;
+        TextView textTime;
+        StoryView storyView;
+        Button addStroy;
+        ImageView dp;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            storyView = itemView.findViewById(R.id.storyView);
+            textTime=itemView.findViewById(R.id.text_time);
+            textName=itemView.findViewById(R.id.text_name);
+            addStroy=itemView.findViewById(R.id.btn_addStory);
+            dp=itemView.findViewById(R.id.profilepic);
+
+        }
+    }
+
+
+}
