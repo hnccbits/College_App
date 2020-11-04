1ec638cafc1c2421aaac67a82ca4b4d8e2855ab7
anandshivam44 <anand.shivam44@yahoo.com>
2020-11-04 20:22:41 +0530

    Added cool nav drawer

--- app/src/main/java/bitsindri/hncc/collegeapp/Activities/MainActivity.java
+++ app/src/main/java/bitsindri/hncc/collegeapp/Activities/MainActivity.java
@@ -10,48 +10,136 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.navigation.NavigationView;
+import com.shrikanthravi.customnavigationdrawer2.widget.SNavigationDrawer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import bitsindri.hncc.collegeapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DrawerLayout drawer;
-    private NavigationView navigationView;
-    private Toolbar toolbar;
-    private ActionBarDrawerToggle toggle;
+    SNavigationDrawer sNavigationDrawer;
+    int color1=0;
+    Class fragmentClass;
+    public static Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        if(getSupportActionBar()!=null) {
+            getSupportActionBar().hide();
+        }
+        sNavigationDrawer = findViewById(R.id.navigationDrawer);
+        List<com.shrikanthravi.customnavigationdrawer2.data.MenuItem> menuItems = new ArrayList<>();
+        menuItems.add(new com.shrikanthravi.customnavigationdrawer2.data.MenuItem("Home",R.drawable.news_bg));
+        menuItems.add(new com.shrikanthravi.customnavigationdrawer2.data.MenuItem("Marketplace",R.drawable.feed_bg));
+        menuItems.add(new com.shrikanthravi.customnavigationdrawer2.data.MenuItem("Messages",R.drawable.message_bg));
+        menuItems.add(new com.shrikanthravi.customnavigationdrawer2.data.MenuItem("Music",R.drawable.music_bg));
+        sNavigationDrawer.setMenuItemList(menuItems);
+        fragmentClass =  HomeFragment.class;
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out).replace(R.id.frameLayout, fragment).commit();
+        }
 
-        navigationView = findViewById(R.id.navigation_drawer);
-        drawer = findViewById(R.id.drawer);
-
-        toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open, R.string.close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
-
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+        sNavigationDrawer.setOnMenuItemClickListener(new SNavigationDrawer.OnMenuItemClickListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id = item.getItemId();
-                if(id == R.id.home) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
-                }else if(id == R.id.marketplace) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MarketplaceFragment()).commit();
+            public void onMenuItemClicked(int position) {
+                System.out.println("Position "+position);
+
+                switch (position){
+                    case 0:{
+//                        color1 = ;
+                        fragmentClass = HomeFragment.class;
+                        break;
+                    }
+                    case 1:{
+//                        color1 = R.color.orange;
+                        fragmentClass = MarketplaceFragment.class;
+                        break;
+                    }
+                    case 2:{
+//                        color1 = R.color.green;
+                        fragmentClass = HomeFragment.class;
+                        break;
+                    }
+                    case 3:{
+//                        color1 = R.color.blue;
+                        fragmentClass = MarketplaceFragment.class;
+                        break;
+                    }
+
                 }
-                drawer.closeDrawer(GravityCompat.START);
-                return true;
+                sNavigationDrawer.setDrawerListener(new SNavigationDrawer.DrawerListener() {
+
+                    @Override
+                    public void onDrawerOpened() {
+
+                    }
+
+                    @Override
+                    public void onDrawerOpening(){
+
+                    }
+
+                    @Override
+                    public void onDrawerClosing(){
+                        System.out.println("Drawer closed");
+
+                        try {
+                            fragment = (Fragment) fragmentClass.newInstance();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                        if (fragment != null) {
+                            FragmentManager fragmentManager = getSupportFragmentManager();
+                            fragmentManager.beginTransaction().setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out).replace(R.id.frameLayout, fragment).commit();
+
+                        }
+                    }
+
+                    @Override
+                    public void onDrawerClosed() {
+
+                    }
+
+                    @Override
+                    public void onDrawerStateChanged(int newState) {
+                        System.out.println("State "+newState);
+                    }
+                });
             }
         });
+
+
+
+//        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+//
+//        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                int id = item.getItemId();
+//                if(id == R.id.home) {
+//                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+//                }else if(id == R.id.marketplace) {
+//                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MarketplaceFragment()).commit();
+//                }
+//                drawer.closeDrawer(GravityCompat.START);
+//                return true;
+//            }
+//        });
         
     }
 
