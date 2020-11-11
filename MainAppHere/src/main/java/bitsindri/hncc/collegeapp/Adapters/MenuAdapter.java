422e53385078fcaac64a3adda7cfe2567503cf1b
Anubhaw19 <anubhawsharma99@gmail.com>
2020-11-11 20:13:15 +0530

    Menu Added

---
+++ MainAppHere/src/main/java/bitsindri/hncc/collegeapp/Adapters/MenuAdapter.java
@@ -0,0 +1,86 @@
+package bitsindri.hncc.collegeapp.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import bitsindri.hncc.collegeapp.Custom_Classes.menuClass;
+import bitsindri.hncc.collegeapp.GetterAndSetter.marketplace;
+import bitsindri.hncc.collegeapp.R;
+
+public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.ExampleViewHolder> {
+    private calluser callusers;
+    private Context mContext ;
+    private ArrayList<menuClass> marketplaceList ;
+    boolean click=false;
+    int temp=0;
+
+
+    public MenuAdapter(ArrayList<menuClass> marketplaceList, calluser callusers) {
+        this.marketplaceList = marketplaceList;
+       this.callusers= callusers;
+
+    }
+
+    @NonNull
+    @Override
+    public ExampleViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_menu,parent,false);
+        ExampleViewHolder evh = new ExampleViewHolder(v , callusers);
+        return evh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ExampleViewHolder holder, int position) {
+
+
+       holder.itemCategory.setText(marketplaceList.get(position).getItemCategory());
+       holder.menu_icon.setImageResource(marketplaceList.get(position).getImgResource());
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return marketplaceList.size();
+    }
+    public  class ExampleViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        TextView itemCategory;
+        ImageView menu_icon;
+        calluser callusers;
+
+
+        public ExampleViewHolder(@NonNull View itemView, calluser calluser) {
+            super(itemView);
+            itemCategory = itemView.findViewById(R.id.item_category);
+            menu_icon=itemView.findViewById(R.id.menu_icon);
+            this.callusers = calluser;
+
+            itemView.setOnClickListener(this);
+
+        }
+
+        @Override
+        public void onClick(View view) {
+            //Toast.makeText(mContext,"hello",Toast.LENGTH_SHORT).show();
+            callusers.productDetails(getAdapterPosition());
+
+        }
+    }
+
+    //Interface containing method......
+    public interface calluser{
+        void productDetails(int position);
+    }
+}
