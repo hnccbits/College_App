ac1b0787412b5a233a32849ac04a96e7ac45ce32
Anubhaw19 <anubhawsharma99@gmail.com>
2020-12-17 23:39:55 +0530

    shimmer added

--- MainAppHere/src/main/java/bitsindri/hncc/collegeapp/Custom_Classes/Data.java
+++ MainAppHere/src/main/java/bitsindri/hncc/collegeapp/Custom_Classes/Data.java
@@ -3,39 +3,74 @@ package bitsindri.hncc.collegeapp.Custom_Classes;
 import com.google.gson.annotations.SerializedName;
 
 public class Data {
-    @SerializedName("ID")
-    private  String id;
+//    @SerializedName("ID")
+//    private  String id;
 
 
-    @SerializedName("name")
-    private  String name;
-
-
-    @SerializedName("branch")
+//    @SerializedName("name")
+//    private  String name;
+//    @SerializedName("branch")
+//    private  String branch;
+//    @SerializedName("url")
+//    private  String url;
+//    @SerializedName("category")
+//    private  String category;
+//
+//    public Data(String name, String branch, String category, String url) {
+//        this.name = name;
+//        this.branch = branch;
+//        this.url = url;
+//        this.category = category;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public String getBranch() {
+//        return branch;
+//    }
+//
+//    public String getUrl() {
+//        return url;
+//    }
+//
+//    public String getCategory() {
+//        return category;
+//    }
+        @SerializedName("branch")
     private  String branch;
 
+    @SerializedName("sub")
+    private  String sub;
 
-    public String getId() {
-        return id;
-    }
 
-    public void setId(String id) {
-        this.id = id;
-    }
+    @SerializedName("sem")
+    private  String sem;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    @SerializedName("url")
+    private  String url;
 
     public String getBranch() {
         return branch;
     }
 
-    public void setBranch(String branch) {
+    public String getSub() {
+        return sub;
+    }
+
+    public String getSem() {
+        return sem;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Data(String branch, String sub, String sem, String url) {
         this.branch = branch;
+        this.sub = sub;
+        this.sem = sem;
+        this.url = url;
     }
 }
