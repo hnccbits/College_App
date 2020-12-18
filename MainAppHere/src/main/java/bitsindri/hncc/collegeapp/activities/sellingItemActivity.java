a6ff982290b17f8ff5a42504694176a2599eea5f
Prayanshu <prayanshubit@gmail.com>
2020-12-18 21:04:34 +0530

    shows user phone number on chat button click in selling item activity

--- MainAppHere/src/main/java/bitsindri/hncc/collegeapp/activities/sellingItemActivity.java
+++ MainAppHere/src/main/java/bitsindri/hncc/collegeapp/activities/sellingItemActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import bitsindri.hncc.collegeapp.R;
@@ -11,6 +13,8 @@ import bitsindri.hncc.collegeapp.R;
 public class sellingItemActivity extends AppCompatActivity {
 
     TextView itemCategory, itemPrice;
+    Button chatButton;
+    Boolean showNumber = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,7 @@ public class sellingItemActivity extends AppCompatActivity {
 
         itemCategory = findViewById(R.id.item_category);
         itemPrice = findViewById(R.id.item_price);
+        chatButton = findViewById(R.id.chat_button);
 
         Intent intent = getIntent();
         String sellingActivityCategory = intent.getExtras().getString("category");
@@ -26,6 +31,19 @@ public class sellingItemActivity extends AppCompatActivity {
 
         itemCategory.setText(sellingActivityCategory);
         itemPrice.setText(sellingActivityPrice);
+
+        chatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(showNumber){
+                    chatButton.setText("+91 987456321");
+                    showNumber = false;
+                }else {
+                    chatButton.setText("Chat");
+                    showNumber = true;
+                }
+            }
+        });
 
     }
 }
